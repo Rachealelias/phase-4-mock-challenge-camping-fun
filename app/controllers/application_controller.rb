c7427494e72f8a43e5c753bf39d61a76c2ffb476
Racheal Amuneke <rachealsamuel708@gmail.com>
2022-04-18 19:23:22 -0400

    commit tree d5b082f5b049cee5fb024cc87f809d479873cd0f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+rescue_from ActiveRecord::RecordNotFound, with: :not_found
+rescue_from ActiveRecord::RecordInvalid, with: :not_processed
+private
 
+def not_found(exception)
+render json: { errors: "#{exception.model} not found"}, status: :not_found
+end
+
+def not_processed(invalid)
+  render json: { errors: [invalid.record.errors]}, status: :unprocessable_entity
+end
 end
